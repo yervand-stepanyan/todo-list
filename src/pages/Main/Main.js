9b20e18d60cb05f85726d3eb90e6b72c7f781135
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2021-02-18 21:55:01 +0100

    Add Title and Input components to Main.js

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,28 @@
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+import Input from '../../components/Input';
+import Title from '../../components/Title';
+import { TITLE } from '../../globals/constants';
+import { useStyles } from './Main.style';
+
 function Main() {
-  return <div>Main works...</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.mainContainer}>
+      <div className={classes.titleContainer}>
+        <Title text={TITLE} />
+      </div>
+      <div className={classes.contentContainer}>
+        <Card className={classes.card}>
+          <CardContent>
+            <Input />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
---
+++ src/pages/Main/Main.style.js
@@ -0,0 +1,27 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles(theme => ({
+  mainContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: theme.color.mainBackgroundColor,
+    boxSizing: 'border-box',
+    minHeight: '100vh',
+    padding: theme.customSpacing.base,
+  },
+  titleContainer: {
+    marginBottom: theme.customSpacing.base,
+  },
+  contentContainer: {
+    height: '80vh',
+    width: '100%',
+    '@media only screen and (min-width: 600px)': {
+      height: '75vh',
+      width: '60%',
+    },
+  },
+  card: {
+    height: 'inherit',
+  },
+}));
