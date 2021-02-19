d9f367407aa43a3176758c783264c3cc5835b9d1
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2021-02-19 18:01:20 +0100

    Add custom properties in theme.js

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,6 +1,8 @@
 import { createMuiTheme } from '@material-ui/core';
 
+const EDIT_PADDING_VALUE = 62;
 const MAIN_SPACING_VALUE = 16;
+const TODO_ITEM_HEIGHT = 56;
 
 const mainBackgroundColor = '#F5F7F9';
 
@@ -9,6 +11,11 @@ export const theme = createMuiTheme({
     mainBackgroundColor,
   },
   customSpacing: {
+    small: `${MAIN_SPACING_VALUE / 2}px`,
     base: `${MAIN_SPACING_VALUE}px`,
+    editTextFieldPaddingRight: `${EDIT_PADDING_VALUE}px`,
+  },
+  size: {
+    todoItemHeight: `${TODO_ITEM_HEIGHT}px`,
   },
 });
