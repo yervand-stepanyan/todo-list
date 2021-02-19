77b770ea09f88f9338d5aeff28ddd5e9989acead
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2021-02-19 21:55:21 +0100

    Add custom styling properties in theme.js

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -5,14 +5,17 @@ const MAIN_SPACING_VALUE = 16;
 const TODO_ITEM_HEIGHT = 56;
 
 const checkedDescriptionColor = '#b2b2b2';
+const leftItemLabelColor = '#8c8c8c';
 const mainBackgroundColor = '#F5F7F9';
 
 export const theme = createMuiTheme({
   color: {
     checkedDescriptionColor,
+    leftItemLabelColor,
     mainBackgroundColor,
   },
   customSpacing: {
+    xSmall: `${MAIN_SPACING_VALUE / 4}px`,
     small: `${MAIN_SPACING_VALUE / 2}px`,
     base: `${MAIN_SPACING_VALUE}px`,
     editTextFieldPaddingRight: `${EDIT_PADDING_VALUE}px`,
