e093138fbbb4df9f2a8623a9f6ffc878d545d5d7
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2021-02-19 11:22:56 +0100

    WIP in Main.js and Input.js

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,11 +1,34 @@
+import { useState } from 'react';
+
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 
 import { BUTTON_LABEL, INPUT } from '../../globals/constants';
+import { useStore } from '../../store/use-store';
 import { useStyles } from './Input.style';
 
 function Input() {
   const classes = useStyles();
+  const [value, setValue] = useState('');
+  const { handleAddTodo } = useStore();
+
+  const handleInputChange = event => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (value) {
+      handleAddTodo(value);
+
+      setValue('');
+    }
+  };
+
+  const handleSubmitOnEnter = event => {
+    if (event.key === 'Enter') {
+      handleSubmit();
+    }
+  };
 
   return (
     <div className={classes.inputContainer}>
@@ -14,7 +37,10 @@ function Input() {
           className={classes.textField}
           fullWidth
           id="outlined-full-width"
+          onChange={e => handleInputChange(e)}
+          onKeyDown={e => handleSubmitOnEnter(e)}
           placeholder={INPUT.placeholder}
+          value={value}
           variant="outlined"
         />
       </div>
@@ -22,6 +48,7 @@ function Input() {
         <Button
           className={classes.button}
           color="primary"
+          onClick={handleSubmit}
           size="large"
           variant="contained"
         >
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,13 +1,29 @@
+import { useState } from 'react';
+import uuid from 'react-uuid';
+
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 
 import Input from '../../components/Input';
+import StoreContext from '../../store/context';
 import Title from '../../components/Title';
 import { TITLE } from '../../globals/constants';
+import Todolist from '../../components/Todolist';
 import { useStyles } from './Main.style';
 
 function Main() {
   const classes = useStyles();
+  const [todolist, setTodolist] = useState([]);
+
+  const handleAddTodo = description => {
+    const newTodo = {
+      description,
+      done: false,
+      id: uuid(),
+    };
+
+    setTodolist([...todolist, newTodo]);
+  };
 
   return (
     <div className={classes.mainContainer}>
@@ -17,7 +33,19 @@ function Main() {
       <div className={classes.contentContainer}>
         <Card className={classes.card}>
           <CardContent>
-            <Input />
+            <StoreContext.Provider
+              value={{
+                handleAddTodo,
+                todolist,
+              }}
+            >
+              <div>
+                <Input />
+              </div>
+              <div>
+                <Todolist />
+              </div>
+            </StoreContext.Provider>
           </CardContent>
         </Card>
       </div>
